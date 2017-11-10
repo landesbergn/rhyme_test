5250334ae3253ae31fb2445d7d5ae37ddf972bf6
landesbergn <noah.landesberg@gmail.com>
2017-11-10 16:54:45 -0500

    add some more functions

--- first_pass.R
+++ first_pass.R
@@ -10,7 +10,7 @@ datamuse_api <- function(path) {
     stop("API did not return json", call. = FALSE)
   }
   
-  parsed <- jsonlite::fromJSON(content(resp, "text"))
+  parsed <- jsonlite::fromJSON(content(resp, "text", encoding = "UTF-8"))
   
   structure(
     list(
@@ -28,15 +28,43 @@ print.datamuse_api <- function(x, ...) {
   invisible(x)
 }
 
-get_rhyme <- function(word = "test") {
-  x <- datamuse_api(paste0("/words?rel_rhy=", word))
+get_data <- function(full_path) {
+  x <- datamuse_api(full_path)
   content <- x$content
   
   if ("score" %in% colnames(content)) {
-    content <- content %>% top_n(10, score)
+    content <- content %>% head(10)
   }
-
+  
   return(content)
 }
 
+get_rhyme <- function(word = "test") {
+  get_data(paste0("/words?rel_rhy=", word))
+}
+
+get_means_like <- function(word = "test") {
+  get_data(paste0("/words?ml=", word))
+}
+
+get_sounds_like <- function(word = "test") {
+  get_data(paste0("/words?sl=", word))
+}
+
+get_spelled_like <- function(word = "test") {
+  get_data(paste0("/words?sp=", word))
+}
+
+get_spelled_like <- function(word = "test") {
+  get_data(paste0("/words?sp=", word))
+}
+
+get_other_related <- function(word = "test", code = "jja") {
+  get_data(paste0("/words?rel_", code, "=", word))
+}
+
 get_rhyme("forge")
+get_means_like("forge")
+get_sounds_like("forge")
+get_spelled_like("forge")
+get_other_related("forge", code = "jja")
